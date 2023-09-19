d3da1551c19d3aa6044235663ce8063816a482e9
jo.zeller@protonmail.com <johannes_zeller@sluz.ch>
2023-09-19 20:38:59 +0200

    scheduling or asynchronization isn't necessary yet

--- src/main/java/tech/bison/trainee/CloudCondensePocApplication.java
+++ src/main/java/tech/bison/trainee/CloudCondensePocApplication.java
@@ -2,12 +2,8 @@ package tech.bison.trainee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
-@EnableScheduling
 public class CloudCondensePocApplication {
 
   public static void main(String[] args) {
